06b317e0456e4aeed73583a5499cbcdbc333af87
Sunil Kumar <apple@Apples-MacBook-Pro.local>
2025-09-20 10:50:54 +0530

    ch

--- demo/test.go
+++ demo/test.go
@@ -1,35 +1,7 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
-)
+import "fmt"
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		if req.Method != http.MethodPost {
-			http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
-			return
-		}
-
-		// Read body
-		body, err := io.ReadAll(req.Body)
-		data := json.RawMessage(body)
-		if err != nil {
-			http.Error(w, "Failed to read request body", http.StatusInternalServerError)
-			return
-		}
-		defer req.Body.Close()
-
-		// Log the body to server console
-		fmt.Println("Received Body:", data)
-
-		// Send response back to client
-		fmt.Fprintf(w, "You sent: %s", string(body))
-	})
-
-	fmt.Println("Server running on http://localhost:3000")
-	http.ListenAndServe(":3000", nil)
+	fmt.Println(x)
 }
