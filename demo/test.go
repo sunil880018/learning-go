e6a9d7d15f46871c7fbfea047c7ed48ebd78f9fd
Sunil Kumar <apple@Apples-MacBook-Pro.local>
2025-09-24 06:36:27 +0530

    kafka

--- demo/test.go
+++ demo/test.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println(x)
+	ch := make(chan int)
+	for i := 0; i < 3; i++ {
+		go func(i int) {
+			ch <- i
+			fmt.Println("Sent:", i)
+		}(i)
+	}
+	// time.Sleep(1 * time.Second)
+	// close(ch)
+	for i := 0; i < 3; i++ {
+		v := <-ch
+		fmt.Println("Received:", v)
+	}
 }
--- kafka/README.md
+++
@@ -1,3 +0,0 @@
-# Episode #44: Go Web Development: Adding Middleware to HTTP Server
-
-[Episode link](https://www.codeheim.io/courses/Episode-44-Mastering-Kafka-with-Golang-A-Beginners-Guide-6684b59a52e98b52484e560d)
